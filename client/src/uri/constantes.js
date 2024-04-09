b5c441db4df7a7880604f649d4a85ebf253124b1
proyectos-developer <proyectos@developer-ideas.com>
2024-04-09 10:48:57 -0500

    V1.0.8

--- client/src/uri/constantes.js
+++ client/src/uri/constantes.js
@@ -1,7 +1,7 @@
 export const constantes = () => {
     return {
         url_principal: [
-            {url: 'http://localhost:3001/api'}
+            {url: 'https://developer-ideas.com/api'}
         ]
     }
 }
